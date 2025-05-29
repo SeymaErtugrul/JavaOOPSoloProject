01781e8c39eaf675859fc5b8f9366665b5617572
Şeyma Ertuğrul <seymaertgrl@gmail.com>
2025-05-29 23:44:48 +0300

    SeymaErtugrul/Seyma/JA-6-Polimorfizm

---
+++ src/Dog.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class Dog extends  Animal
+{
+    @Override
+    public void makeSound() {
+        System.out.println("Hav");
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args) {
+        Cat cat= new Cat();
+        Dog dog=new Dog();
+        Animal animal=new Animal();
+
+        cat.makeSound();
+        dog.makeSound();
+        animal.makeSound();
+    }
+}
