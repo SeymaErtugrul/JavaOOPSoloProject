12c2e4aa52e9f149caef01918e345174839f7a8c
Şeyma Ertuğrul <seymaertgrl@gmail.com>
2025-05-29 23:35:36 +0300

    SeymaErtugrul/Seyma/JA-5-Method-Overriding

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,4 +1,10 @@
+import javax.xml.stream.events.StartDocument;
+
 public class Vehicle {
     public float speed;
 
+    public void Start()
+    {
+        System.out.println("Arac baslatiliyor");
+    }
 }
