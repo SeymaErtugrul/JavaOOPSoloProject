71bbd633a75107a2357ee30b53df0cc1b0d11497
Şeyma Ertuğrul <seymaertgrl@gmail.com>
2025-05-29 23:20:59 +0300

    SeymaErtugrul/Seyma/JA-3-Getter-Setter-Methods

---
+++ src/CarE.java
@@ -0,0 +1,46 @@
+public class CarE
+{
+    private  String carBrand="Audi";
+    private  String carModel="i5";
+    private  String carProductYear="2017";
+
+
+    public String getCarBrand()
+    {
+       return  carBrand;
+    }
+
+    public void setCarBrand(String newBrand)
+    {
+        this.carBrand=newBrand;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String newModel)
+    {
+        this.carModel=newModel;
+    }
+
+    public String getCarProductYear()
+    {
+        return  carProductYear;
+    }
+
+    public void setCarProductYear(String newProductYear)
+    {
+        this.carProductYear=newProductYear;
+    }
+    public static void main(String[] args) {
+        CarE carE=new CarE();
+        carE.setCarBrand("Ferrari");
+        carE.setCarModel("99GE");
+        carE.setCarProductYear("2019");
+
+        System.out.println(carE.getCarBrand());
+        System.out.println(carE.getCarModel());
+        System.out.println(carE.getCarProductYear());
+    }
+}
