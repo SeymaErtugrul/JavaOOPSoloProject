9c83e64e2c0bb0cb98a784351357221c92b30271
Şeyma Ertuğrul <seymaertgrl@gmail.com>
2025-06-01 23:23:03 +0300

    SeymaErturul/Seyma/JA-8-Static-Use

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,14 @@ public class Main
         //Method Overloading
         System.out.println(calculator.add(67,54));
         System.out.println(calculator.add(345,467,3));
+
+        //Static metod kullanımı
+       System.out.println(addTwoNums(45,2312));
+    }
+
+    public static int addTwoNums(int num1, int num2)
+    {
+        System.out.println(num1 + " ve " + num2 + " sayilarini toplami ");
+        return num1+num2;
     }
 }
